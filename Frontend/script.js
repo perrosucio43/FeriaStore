8f834d476b63686a74271e3d085d16097a0876c6
Matias <matiasdarosa742@gmail.com>
2026-04-28 20:28:28 -0300

    go

---
+++ Frontend/script.js
@@ -0,0 +1,16 @@
+const nav=document.querySelector(".encabezado");
+
+let current=0;
+
+window.addEventListener("scroll", ()=>{
+console.log("scroll");
+let actual=window.scrollY;
+if(actual>current){
+nav.classList.add("active")
+}
+else{
+    nav.classList.remove("active");
+}
+current=actual;
+
+});
